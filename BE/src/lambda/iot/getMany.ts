74a6d7f3c4cbfbafdefaff663401077aca972013
Jakub Manak <jakub.manak@plus4u.net>
2023-03-13 21:15:05 +0100

    DB measurement added location
    Co-authored-by: CplPrince <cplprince@users.noreply.github.com>

--- BE/src/lambda/iot/getMany.ts
+++ BE/src/lambda/iot/getMany.ts
@@ -1,34 +1,39 @@
 import { status200 } from '../../libs/http/status200';
+import { status400 } from '../../libs/http/status400';
 import { useDB, withDB } from '../../libs/wrapper/withDB';
 import { withHttp } from '../../libs/wrapper/withHttp';
-import { withUser, useUser } from '../../libs/wrapper/withUser';
 import { Lambda } from '../../../../types/lambda';
 import { Op } from 'sequelize';
-import { Mesurement } from 'libs/database/models/mesurement';
 
 export const handler: Lambda = withHttp(
-	withDB(
-		withUser(async ({ body }) => {
-			const user = useUser();
-			const payload = await user.getPayload();
-			const token = await user.getToken(payload);
-			const db = await useDB();
-			// TO FIX: const { Mesurement } = await useDB();
-			const { value, type } = JSON.parse(body); // získání dat z requestu dny
-			if (!value || !type) return status400();
+	withDB(async ({ body }) => {
+		//const user = useUser();
+		// const payload = await user.getPayload();
+		// const token = await user.getToken(payload);
+		console.log(body);
+		if (!body) return status400();
+		const { type, userId, days } = JSON.parse(body); // získání dat z requestu dny
+		if (!type || !userId || !days) return status400();
 
-			// Získání teplotních dat z databáze v rozmezí od 1 minuty do 1 dne
-			const temperatureData = await Mesurement.findAll({
-				//pipeline
-				where: {
-					createdAt: {
-						[Op.lte]: new Date(new Date().getTime() - 60 * 1000 * 60 * 24 * days), // od 1 minuty zpět
-					},
+		const { Mesurement } = await useDB();
+
+		// Získání teplotních dat z databáze v rozmezí od 1 minuty do 1 dne
+		const temperatureData = await Mesurement.findAll({
+			//pipeline
+			where: {
+				createdAt: {
+					[Op.gte]: new Date(new Date().getTime() - 60 * 1000 * 60 * 24 * days), // od 1 minuty zpět
 				},
-				order: [['createdAt', 'DESC']],
-			});
+				type: {
+					[Op.eq]: type,
+				},
+				userId: {
+					[Op.eq]: userId,
+				},
+			},
+			order: [['createdAt', 'DESC']],
+		});
 
-			return status200({ data: { temperatureData } });
-		}),
-	),
+		return status200({ data: { temperatureData } });
+	}),
 );
--- BE/src/lambda/iot/post.ts
+++ BE/src/lambda/iot/post.ts
@@ -2,9 +2,8 @@ import { status200 } from '../../libs/http/status200';
 import { status400 } from '../../libs/http/status400';
 import { useDB, withDB } from '../../libs/wrapper/withDB';
 import { withHttp } from '../../libs/wrapper/withHttp';
-import { withUser, useUser } from '../../libs/wrapper/withUser';
+import {  useUser } from '../../libs/wrapper/withUser';
 import { Lambda } from '../../../../types/lambda';
-import { User } from 'libs/database/models/user';
 import { withRole } from 'libs/wrapper/withRole';
 import { Role } from 'libs/database/models/user';
 
@@ -16,8 +15,8 @@ export const handler: Lambda = withHttp(
 		withRole([Role.IOT, Role.USER], async ({ body }) => {
 			// získání dat z requestu
 			if (!body) return status400();
-			const { value, type } = JSON.parse(body);
-			if (!value || !type) return status400();
+			const { value, type, location } = JSON.parse(body);
+			if (!value || !type || !location) return status400();
 
 			// nalezení uživatele
 			const user = useUser();
@@ -28,6 +27,7 @@ export const handler: Lambda = withHttp(
 				value,
 				type,
 				userId: user.id,
+				location,
 			});
 
 			// výsledek
