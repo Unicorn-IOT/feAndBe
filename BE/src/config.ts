ecceaa316b3bacc3b4211c1e7317a521c2eef318
Jakub Manak <jakub.manak@plus4u.net>
2023-03-14 12:37:31 +0100

    config.ts minor adjustment

--- BE/src/config.ts
+++ BE/src/config.ts
@@ -10,3 +10,17 @@ export const db = {
 	password: isOffline && localDB ? '' : '',
 	port: 3306,
 };
+
+/*----------------Cloud Setup----------------------*/
+
+// export const isOffline = false;
+
+// export const localDB = false;
+
+// export const db = {
+// 	host: isOffline ? 'localhost' : '212.237.39.159',
+// 	database: 'w20hxd056y',
+// 	user: isOffline && localDB ? 'root' : 'm6gb7d3z5v',
+// 	password: isOffline && localDB ? 'admin' : 'zuUBUAv@6i]IB0uU',
+// 	port: 3306,
+// };
