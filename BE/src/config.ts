65348eeb480f0aee9e03b4459bcfeb26883e1880
Jakub Manak <jakub.manak@plus4u.net>
2023-03-15 22:20:16 +0100

    iot Reg/Log/Get - halfway there

--- BE/src/config.ts
+++ BE/src/config.ts
@@ -1,9 +1,24 @@
-export const isOffline = true;
+/**local*/
+//LOCAL Setup
+export const isOffline = false;
+
+export const localDB = true;
 
 export const db = {
-	host: 'unicorniot.cd98mbmqu8a5.eu-west-1.rds.amazonaws.com',
-	database: 'unicorniot',
-	user: 'admin',
-	password: 'rootroot',
+	host: isOffline ? 'localhost' : '127.0.0.1',
+	database: 'test',
+	user: isOffline && localDB ? 'root' : 'root',
+	password: isOffline && localDB ? '' : '',
 	port: 3306,
 };
+
+/*CLOUD Setting*/
+// export const isOffline = true;
+
+// export const db = {
+// 	host: 'unicorniot.cd98mbmqu8a5.eu-west-1.rds.amazonaws.com',
+// 	database: 'unicorniot',
+// 	user: 'admin',
+// 	password: 'rootroot',
+// 	port: 3306,
+// };
---
+++ BE/src/lambda/iot/iotLogin.ts
@@ -0,0 +1,27 @@
+import { status200 } from '../../libs/http/status200';
+import { status400 } from '../../libs/http/status400';
+import { status403 } from '../../libs/http/status403';
+import { status404 } from '../../libs/http/status404';
+import { useDB, withDB } from '../../libs/wrapper/withDB';
+import { withHttp } from '../../libs/wrapper/withHttp';
+import { Lambda } from '../../../../types/lambda';
+
+export const handler: Lambda = withHttp(
+	withDB(async ({ body }) => {
+		if (!body) return status400();
+
+		const { email, password, name } = JSON.parse(body);
+		if (!email || !password || !name) return status400();
+
+		const { User } = await useDB();
+		const user = await User.findByNameIot(name);
+		if (!user) return status404();
+
+		const verified = user.verifyPassword(password);
+		if (!verified) return status403();
+
+		const token = await user.getToken();
+
+		return status200({ data: { token } });
+	}),
+);
