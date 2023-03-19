a6a81282445807af1895d7c332c76719d9d7ae27
Tomáš Endrych <tomas.endrych@superkoders.com>
2023-03-19 19:21:14 +0100

    type fix

--- BE/src/lambda/iot/get.ts
+++ BE/src/lambda/iot/get.ts
@@ -2,7 +2,7 @@ import { status200 } from '../../libs/http/status200';
 import { withDB } from '../../libs/wrapper/withDB';
 import { withHttp } from '../../libs/wrapper/withHttp';
 import { Lambda } from '../../../../types/lambda';
-import { Mesurement } from 'libs/database/models/mesurement';
+import { Mesurement } from './../../libs/database/models/mesurement';
 
 // Endpoint pro získání posledního záznamu z DB
 
