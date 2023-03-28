1c483b89a1e689be7c9caa628d4d8beffa0d7482
Jakub Manak <jakub.manak@plus4u.net>
2023-03-28 21:01:00 +0200

    iot/login resolved

--- BE/src/lambda/iot/auth/login/post.ts
+++ BE/src/lambda/iot/auth/login/post.ts
@@ -5,40 +5,23 @@ import { status404 } from '../../../../libs/http/status404';
 import { useDB, withDB } from '../../../../libs/wrapper/withDB';
 import { withHttp } from '../../../../libs/wrapper/withHttp';
 import { Lambda } from '../../../../../../types/lambda';
-import { withUser, useUser } from '../../../../libs/wrapper/withUser';
 
 export const handler: Lambda = withHttp(
-	withDB(
-		withUser(async ({ body }) => {
-			if (!body) return status400();
-			console.log('body', body);
+	withDB(async ({ body }) => {
+		if (!body) return status400();
 
-			const user = useUser();
-			const emailId = user.emailId;
+		const { name, password } = JSON.parse(body);
+		if (!name || !password) return status400();
 
-			const { name, password } = JSON.parse(body);
-			if (!name || !password) return status400();
+		const { User } = await useDB();
+		const iotName = await User.findByName(name);
+		if (!iotName) return status404();
 
-			const { User } = await useDB();
-			const iot = await User.findWithEmail(emailId);
-			if (!iot) return status404();
+		const verified = iotName.verifyPassword(password);
+		if (!verified) return status403();
 
-			const iotName = await User.findByName(name);
-			if (!iotName) return status404();
+		const token = await iotName.getToken();
 
-			const emailIdIot = iotName.emailId;
-
-			const { Email } = await useDB();
-			const email = await Email.findWithEmailId(emailId);
-			if (!email) return status404();
-			if (emailId !== emailIdIot) return status400();
-
-			const verified = iot.verifyPassword(password);
-			if (!verified) return status403();
-
-			const token = await iot.getToken();
-
-			return status200({ data: { token } });
-		}),
-	),
+		return status200({ data: { token } });
+	}),
 );
