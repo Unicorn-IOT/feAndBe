81ad3ed4995ccb5c855b132a2bda6d98cabbaf2b
Tomáš Endrych <tomas.endrych@superkoders.com>
2023-04-05 19:58:04 +0200

    fix

--- FE/src/pages/404.tsx
+++ FE/src/pages/404.tsx
@@ -1,17 +1,7 @@
 import { NextPage } from 'next';
-import { wrapper } from '../store';
-import { prepopulateUserInfo } from '../store/server/prepopulateUserInfo';
-import { useServerLoggedOutRedirect } from '../store/server/useServerLoggedOutRedirect';
 
 const Error404: NextPage = () => {
 	return <div>page not found... Patriku, dolad tuhle stránku pls</div>;
 };
 
-export const getServerSideProps = wrapper.getServerSideProps((store) => async (context) => {
-	prepopulateUserInfo(store, context);
-	return {
-		props: {},
-		redirect: useServerLoggedOutRedirect(context),
-	};
-});
 export default Error404;
