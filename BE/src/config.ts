4630180b5b23d2ae5bb892e215462dc8415901bd
kawalski66 <100319125+kawalski66@users.noreply.github.com>
2023-04-06 18:28:27 +0200

    Be mana2 (#4)
    * iot register
    
    * iotGet
    
    * iot Reg/Log/Get - halfway there
    
    * iotReg doesnt make refernce on emailId
    
    * iotReg shit
    
    * shit2
    
    * iotRegister; DB relations; local config
    
    * fixed the iotRegister
    
    * iotLogin DONE
    
    * iotRegister DONE
    
    * iotRemove
    
    * code cleaned
    
    * for merge and deploy
    
    * lambda structure; minor code change according boss
    
    * merge conflict solved
    
    * iot/login resolved
    
    * getUsers issue
    
    * getUsers isssue2
    
    * reset; login; register; models email & user
    
    * createIot method
    
    * add model mesurement date attribute
    
    * add date to iot/data/post
    
    * add date&location to iot/data/get
    
    * config for cloud
    
    * unique index name
    
    * config for cloud
    
    * iot/data/getMany add interval to params
    
    * shit3
    
    * getMany granurality
    
    * iot/data/post only role.iot
    
    * iot/data/getMany fix
    
    * cloud setting
    
    ---------
    
    Co-authored-by: Jakub Manak <jakub.manak@plus4u.net>
    Co-authored-by: Tomáš Endrych <tomas.endrych@superkoders.com>

--- BE/src/config.ts
+++ BE/src/config.ts
@@ -1,3 +1,16 @@
+// LOCAL Setup
+// export const isOffline = false;
+// export const localDB = false;
+
+// export const db = {
+// 	host: isOffline ? 'localhost' : '127.0.0.1',
+// 	database: 'test',
+// 	user: isOffline && localDB ? 'root' : 'root',
+// 	password: isOffline && localDB ? '' : '',
+// 	port: 3306,
+// };
+
+/*CLOUD Setting*/
 export const isOffline = true;
 
 export const db = {
