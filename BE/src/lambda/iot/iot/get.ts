0f826bcc1d394fd6d3c966566402d7979113f6c4
Jakub Manak <jakub.manak@plus4u.net>
2023-04-14 22:32:35 +0200

    FOR PATRIK made with love

--- BE/src/lambda/iot/iot/get.ts
+++ BE/src/lambda/iot/iot/get.ts
@@ -3,6 +3,7 @@ import { withDB } from '../../../libs/wrapper/withDB';
 import { withHttp } from '../../../libs/wrapper/withHttp';
 import { Lambda } from '../../../../../types/lambda';
 import { Role, User } from '../../../libs/database/models/user';
+import { Email, Mesurement } from '../../../libs/database/model';
 
 export const handler: Lambda = withHttp(
 	withDB(async () => {
@@ -10,8 +11,41 @@ export const handler: Lambda = withHttp(
 			where: { role: Role.IOT },
 		});
 
+		const iotResult = [];
+		for (const user of iotUsers) {
+			const name = await User.findOne({
+				where: { id: user.emailId },
+			});
+			const ownerName = name?.name ?? '';
+
+			const email = await Email.findOne({
+				where: { id: user.emailId },
+			});
+			const ownerEmail = email?.email ?? '';
+
+			const ownerUser = await User.findByEmail(ownerEmail);
+			const ownerUserId = ownerUser?.id ?? '';
+
+			const measurement = await Mesurement.findOne({
+				where: { userId: user.id },
+			});
+			const location = measurement?.location ?? '';
+
+			iotResult.push({
+				id: user.id,
+				name: user.name,
+				ownerName,
+				ownerId: ownerUserId,
+				ownerEmail,
+				ownerEmailId: user.emailId,
+				location,
+				createdAt: user.createdAt,
+				updatedAt: user.updatedAt,
+			});
+		}
+
 		return status200({
-			data: { iotUsers },
+			data: { iotResult },
 		});
 	}),
 );
