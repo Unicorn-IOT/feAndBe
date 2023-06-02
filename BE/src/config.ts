156c7724574dce9e0eb524ee2d0541a7a1d3d4f5
Jakub Manak <jakub.manak@plus4u.net>
2023-06-02 17:53:26 +0200

    Fix iot/iot/get

--- BE/src/config.ts
+++ BE/src/config.ts
@@ -1,7 +1,7 @@
 // LOCAL Setup
-// export const isOffline = false;
-// export const localDB = false;
-
+// export const isOffline = true;
+// export const localDB = true;
+//
 // export const db = {
 // 	host: isOffline ? 'localhost' : '127.0.0.1',
 // 	database: 'test',
