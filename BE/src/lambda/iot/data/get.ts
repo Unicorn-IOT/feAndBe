c6dbf55d7cec04038877bb0fbb1a1221a8a4dd1e
Tomáš Endrych <tomas.endrych@superkoders.com>
2023-06-02 20:38:33 +0200

    iot/data/get

--- BE/src/lambda/iot/data/get.ts
+++ BE/src/lambda/iot/data/get.ts
@@ -3,18 +3,29 @@ import { withDB } from '../../../libs/wrapper/withDB';
 import { withHttp } from '../../../libs/wrapper/withHttp';
 import { Lambda } from '../../../../../types/lambda';
 import { Mesurement } from '../../../libs/database/models/mesurement';
+import { z } from 'zod';
+import { validation } from 'libs/validation';
+import { status400 } from 'libs/http/status400';
 
 // Endpoint pro získání posledního záznamu z DB
+const Schema = z.object({
+	userId: z.string(),
+});
 
 export const handler: Lambda = withHttp(
-	withDB(async () => {
+	withDB(async ({ queryStringParameters }) => {
+		if (!queryStringParameters) return status400();
+
+		const { userId } = validation(Schema, queryStringParameters);
+
 		const temperatureMeasurement = await Mesurement.findOne({
-			where: { type: 'temperature' },
-			order: [['createdAt', 'DESC']],
+			where: { type: 'temperature', userId },
+			order: [['date', 'DESC']],
 		});
+
 		const humidityMeasurement = await Mesurement.findOne({
-			where: { type: 'humidity' },
-			order: [['createdAt', 'DESC']],
+			where: { type: 'humidity', userId },
+			order: [['date', 'DESC']],
 		});
 
 		return status200({
