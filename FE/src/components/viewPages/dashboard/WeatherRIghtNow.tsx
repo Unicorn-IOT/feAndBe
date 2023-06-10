a87576ef77b8715b8f93ec2eab817307ed38c4d2
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-06-10 17:30:54 +0200

    datum

--- FE/src/components/viewPages/dashboard/WeatherRIghtNow.tsx
+++ FE/src/components/viewPages/dashboard/WeatherRIghtNow.tsx
@@ -7,35 +7,11 @@ import { useCurrentDataIot } from 'FE/src/hooks/useCurrentDataIot';
 export const WeatherRIghtNow = () => {
 	const { data, isError } = useCurrentDataIot();
 
-	const time = data && data.data.dateTemp ? new Date(data && data.data.dateTemp) : null;
-	const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-	const dayOfWeek = time ? days[time.getDay()] : null;
-	const months = [
-		'January',
-		'February',
-		'March',
-		'April',
-		'May',
-		'June',
-		'July',
-		'August',
-		'September',
-		'October',
-		'November',
-		'December',
-	];
-	const month = time ? months[time.getMonth()] : null;
-	const hours = time ? String(time.getHours()).padStart(2, '0') : '--';
-	const minutes = time ? String(time.getMinutes()).padStart(2, '0') : '--';
-	const seconds = time ? String(time.getSeconds()).padStart(2, '0') : '--';
-
-	const finishTime = `${dayOfWeek ? dayOfWeek + ', ' : ''}${month ? month + ' ' : ''}${hours}:${minutes}:${seconds}`;
-
 	return (
 		<Fragment>
 			<Title>Latest measurement</Title>
 			<Typography component="p" variant="h6" sx={{ flex: 1 }}>
-				{finishTime}
+				{new Date(data?.data.dateTemp || '').toString().split('GMT')[0]}
 			</Typography>
 			<Typography component="p" variant="h6">
 				Humidity:
